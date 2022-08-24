2e29363a004d7c5c1b30a5f940959f3ddec20314
maaammos <moss10612@gmail.com>
2022-08-24 11:45:03 +0700

    test

--- app.js
+++ app.js
@@ -9,15 +9,19 @@ console.log(config);
 
 // middleware
 
-mongoose
-    .connect(config.mongodb.uri, {
-        user: config.mongodb.username,
-        pass: config.mongodb.password,
-        dbName: config.mongodb.dbName,
-        retryWrites: true,
-    })
-    .then(() => app.listen(5000))
-    .then(() => console.log("Connected TO Database and Listening TO Localhost 5000"))
-    .catch((err) => console.log(err));
+const boot = async () => {
+    await mongoose
+        .connect(config.mongodb.uri, {
+            user: config.mongodb.username,
+            pass: config.mongodb.password,
+            dbName: config.mongodb.dbName,
+            retryWrites: true,
+        })
+        .then(() => app.listen(5000))
+        .then(() => console.log("Connected TO Database and Listening TO Localhost 5000"))
+        .catch((err) => console.log(err));
+};
+
+boot();
 
 //----config
